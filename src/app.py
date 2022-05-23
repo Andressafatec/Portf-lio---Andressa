65d6404a1421450ce86280bfb9bbe418593efa3e
Andressafatec <101087740+Andressafatec@users.noreply.github.com>
2022-05-23 00:53:50 -0300

    arrumei a pasta

---
+++ src/app.py
@@ -0,0 +1,20 @@
+from flask import Flask, render_template, url_for
+
+app = Flask(__name__)
+
+@app.route("/")
+@app.route("/index.html")
+def index():
+    return render_template("index.html")
+
+@app.route("/sobre.html")
+def sobre():
+    return render_template("sobre.html")
+
+@app.route("/projetos.html")
+def projetos():
+    return render_template("projetos.html")
+
+@app.route("/contato.html")
+def contato():
+    return render_template("contato.html")
