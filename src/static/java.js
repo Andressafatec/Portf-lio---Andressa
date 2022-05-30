1ed1756d0a1ee5e7134789d51d63bc03aa889c40
Andressafatec <101087740+Andressafatec@users.noreply.github.com>
2022-05-30 03:02:47 -0300

    arrumei o java

--- src/static/java.js
+++ src/static/java.js
@@ -31,55 +31,8 @@ $(document).ready(function() {
 
 });
 
-$(document).ready(function() {
-    /*NAVBAR CODIGO*/
-    $(window).scroll(function() {
-        if (this.scrollY > 10) {
-            $('.navbar').addClass("sticky");
-        } else {
-            $('.navbar').removeClass("sticky");
-        }
-        if (this.scrollY > 500) {
-            $('.scroll-up-btn').addClass("show");
-        } else {
-            $('.scroll-up-btn').removeClass("show");
-        }
-    });
 
-    $('.scroll-up-btn').click(function() {
-        $('html').animate({ scrollTop: 0 });
-    });
 
-    $('.menu-btn').click(function() {
-        $('.navbar .menu').toggleClass("active");
-        $('.menu-btn i').toggleClass("active");
-    });
-
-});
-
-$(document).ready(function() {
-    /*NAVBAR CODIGO*/
-    $(window).scroll(function() {
-        if (this.scrollY > 10) {
-            $('.navbar').addClass("sticky");
-        } else {
-            $('.navbar').removeClass("sticky");
-        }
-        if (this.scrollY > 500) {
-            $('.scroll-up-btn').addClass("show");
-        } else {
-            $('.scroll-up-btn').removeClass("show");
-        }
-    });
-
-    $('.scroll-up-btn').click(function() {
-        $('html').animate({ scrollTop: 0 });
-    });
-
-    $('.menu-btn').click(function() {
-        $('.navbar .menu').toggleClass("active");
-        $('.menu-btn i').toggleClass("active");
-    });
     /*Carrossel*/
     $('.carousel').owlCarousel({
         margin: 20,
@@ -103,33 +56,6 @@ $(document).ready(function() {
 
         }
     });
-});
-
-$(document).ready(function() {
-    /*NAVBAR CODIGO*/
-    $(window).scroll(function() {
-        if (this.scrollY > 10) {
-            $('.navbar').addClass("sticky");
-        } else {
-            $('.navbar').removeClass("sticky");
-        }
-        if (this.scrollY > 500) {
-            $('.scroll-up-btn').addClass("show");
-        } else {
-            $('.scroll-up-btn').removeClass("show");
-        }
-    });
-
-    $('.scroll-up-btn').click(function() {
-        $('html').animate({ scrollTop: 0 });
-    });
-
-    $('.menu-btn').click(function() {
-        $('.navbar .menu').toggleClass("active");
-        $('.menu-btn i').toggleClass("active");
-    });
-
-});
 
 var typed = new Typed(".typing2", {
     strings: ["Andressa."],
